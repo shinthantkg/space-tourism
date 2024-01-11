6a997af9c53403cb0e0c4660b6b60a286158d78a
Shin <shinthantkaung@proton.me>
2024-01-11 20:31:33 +0630

    move navbar to App.jsx instead of putting it in each page component

--- src/components/pages/CurrentPage.jsx
+++ src/components/pages/CurrentPage.jsx
@@ -1,16 +1,25 @@
 import { useContext } from "react";
 import CurrentPageContext from "../../contexts/NavContext.jsx";
 import Home from "./Home.jsx";
+import Destination from "./Destination.jsx";
 
 export default function CurrentPage() {
     const { currentPage } = useContext(CurrentPageContext);
 
     return (
         <>
-            {currentPage === "00" ?
-                <Home />
-                : null
-            }
+            {() => {
+                switch(currentPage) {
+                    case "00":
+                        return <Home />
+                    
+                    case "01":
+                        return <Destination />
+                    
+                    default:
+                        return <Home />
+                }
+            }}
         </>
     )
 }
